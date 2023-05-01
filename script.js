2d789d4a4e0b25db1a5685735af1fcfce65d5bdb
christianmeidyanto <31530811+christianmeidyanto@users.noreply.github.com>
2023-05-01 14:21:54 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 
  var scanner = new Instascan.Scanner({ video: document.getElementById('preview'), scanPeriod: 5, mirror: false });
     scanner.addListener('scan',function(content){
-     sessionStorage.setItem("id", content);
-     window.location.href="/"+content;
+     console.log(content);
     });
     Instascan.Camera.getCameras().then(function (cameras){
         if(cameras.length>0){

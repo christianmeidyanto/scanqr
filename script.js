e1bac6c71900588e7d5144ea0ac5bef44aec5fe4
christianmeidyanto <31530811+christianmeidyanto@users.noreply.github.com>
2023-05-01 11:54:11 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+
+ var scanner = new Instascan.Scanner({ video: document.getElementById('preview'), scanPeriod: 5, mirror: false });
+    scanner.addListener('scan',function(content){
+      alert(content);
+     sessionStorage.setItem("id", content);        //window.location.href=content;
+    });
+    Instascan.Camera.getCameras().then(function (cameras){
+        if(cameras.length>0){
+          if(cameras.length>=3){
+            scanner.start(cameras[2]);
+          }else if(cameras.length>=2){
+            scanner.start(cameras[1]);
+          }else{
+            scanner.start(cameras[0]);
+          }
+        }else{
+            console.error('No cameras found.');
+            alert('No cameras found.');
+        }
+    }).catch(function(e){
+        console.error(e);
+        alert(e);
+    });
